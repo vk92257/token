f54e6c13c15dcfd0e634644e06a510ad6b679029
Vivek <vk92257@gmail.com>
2021-03-24 22:23:23 +0530

    changes

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 // dotenv.config({ path: __dirname + '/config.env' });
-app.get('/', (req, res) => {
-  // res.status(200).json({
-  //   status: 'success',
-  //   message: 'hit the url',
-  // });
+app.get('/', function (req, res) {
   res.status(200).send('hello world');
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('app is running');
 });
+
 // console.log(process.env);
 // console.log(process.env.PORT);
